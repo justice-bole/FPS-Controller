cd95391326f63b0510fce908551db6196003e770
NetworkerLurker <78961523+NetworkerLurker@users.noreply.github.com>
2022-02-07 22:12:33 +0100

    Update 2
    Added horizontal camera movement and vertical/horizontal character movement.

--- FPS Game/Assets/Scripts/Character/CharcterController.cs
+++ FPS Game/Assets/Scripts/Character/CharcterController.cs
@@ -5,11 +5,13 @@ using static Models;
 
 public class CharcterController : MonoBehaviour
 {
+    private CharacterController characterController;
     private DefaultInput defaultInput;
     public Vector2 input_Movement;
     public Vector2 input_View;
 
     private Vector3 newCameraRotation;
+    private Vector3 newCharacterRotation;
 
     [Header("References")]
     public Transform cameraHolder;
@@ -17,7 +19,7 @@ public class CharcterController : MonoBehaviour
     [Header("Settings")]
     public PlayerSettingsModel playerSettings;
     public float viewClampYMin = -70.0f;
-        public float viewClampYMax = 80.0f;
+    public float viewClampYMax = 80.0f;
 
     private void Awake()
     {
@@ -29,24 +31,40 @@ public class CharcterController : MonoBehaviour
         defaultInput.Enable();
 
         newCameraRotation = cameraHolder.localRotation.eulerAngles;
+        newCharacterRotation = transform.localRotation.eulerAngles;
+
+        characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
         CalculateView();
-        //CalculateMovement();
+        CalculateMovement();
     }
 
     private void CalculateView()
     {
-        newCameraRotation.x += playerSettings.ViewYSensitivity * input_View.y * Time.deltaTime;
+        newCharacterRotation.y += playerSettings.ViewXSensitivity * (playerSettings.ViewXInverted ? -input_View.x : input_View.x) * Time.deltaTime;
+        transform.localRotation = Quaternion.Euler(newCharacterRotation);
 
+        newCameraRotation.x += playerSettings.ViewYSensitivity * (playerSettings.ViewYInverted ? input_View.y : -input_View.y) * Time.deltaTime;
         newCameraRotation.x = Mathf.Clamp(newCameraRotation.x, viewClampYMin, viewClampYMax);
 
         cameraHolder.localRotation = Quaternion.Euler(newCameraRotation);
     }
 
-  
+    private void CalculateMovement()
+    {
+        var verticalSpeed = playerSettings.WalkingForwardSpeed * input_Movement.y * Time.deltaTime;
+        var horizontalSpeed = playerSettings.WalkingStrafeSpeed * input_Movement.x * Time.deltaTime;
+
+        var newMovementSpeed = new Vector3(horizontalSpeed, 0, verticalSpeed);
+
+        newMovementSpeed = transform.TransformDirection(newMovementSpeed);
+
+        characterController.Move(newMovementSpeed);
+    }
+
 
 }
 
