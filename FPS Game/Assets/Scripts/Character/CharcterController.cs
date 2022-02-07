8c25772e41e87dd4a4e4641ad75fe4d254399dc9
NetworkerLurker <78961523+NetworkerLurker@users.noreply.github.com>
2022-02-07 21:48:20 +0100

    Update 1
    Added inputs & ability to rotate camera on x axis to simulate looking up & down with mouse.

--- FPS Game/Assets/Scripts/Character/CharcterController.cs
+++ FPS Game/Assets/Scripts/Character/CharcterController.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Models;
 
 public class CharcterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private DefaultInput defaultInput;
+    public Vector2 input_Movement;
+    public Vector2 input_View;
+
+    private Vector3 newCameraRotation;
+
+    [Header("References")]
+    public Transform cameraHolder;
+
+    [Header("Settings")]
+    public PlayerSettingsModel playerSettings;
+    public float viewClampYMin = -70.0f;
+        public float viewClampYMax = 80.0f;
+
+    private void Awake()
     {
-        
+        defaultInput = new DefaultInput();
+
+        defaultInput.Character.Movement.performed += context => input_Movement = context.ReadValue<Vector2>();
+        defaultInput.Character.View.performed += context => input_View = context.ReadValue<Vector2>();
+
+        defaultInput.Enable();
+
+        newCameraRotation = cameraHolder.localRotation.eulerAngles;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        CalculateView();
+        //CalculateMovement();
     }
+
+    private void CalculateView()
+    {
+        newCameraRotation.x += playerSettings.ViewYSensitivity * input_View.y * Time.deltaTime;
+
+        newCameraRotation.x = Mathf.Clamp(newCameraRotation.x, viewClampYMin, viewClampYMax);
+
+        cameraHolder.localRotation = Quaternion.Euler(newCameraRotation);
+    }
+
+  
+
 }
+
---
+++ FPS Game/Assets/Scripts/Models.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Models
+{
+    #region - Player -
+
+    [Serializable]
+    public class PlayerSettingsModel
+    {
+        [Header("View Settings")]
+        public float ViewXSensitivity;
+        public float ViewYSensitivity;
+
+        public bool ViewXInverted;
+        public bool ViewYInverted;
+    }
+
+    #endregion
+
+}
